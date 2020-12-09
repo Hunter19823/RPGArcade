4a4f39d5bc3dbd5d1426a98b22637e3d30836c27
Hunter19823 <hspragg@asu.edu>
2020-12-09 02:49:54 -0700

    Documented Weapon Classes/Enums

--- src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/weapons/Sword.java
+++ src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/weapons/Sword.java
@@ -3,13 +3,29 @@ package main.java.pie.ilikepiefoo2.RPGArcade.Equipment.weapons;
 import main.java.pie.ilikepiefoo2.RPGArcade.Equipment.Slot;
 import main.java.pie.ilikepiefoo2.RPGArcade.Equipment.StatModifier;
 
+/**
+ * Sword Class
+ * Child to the abstract class StatModifier
+ */
 public class Sword extends StatModifier {
 
+    /**
+     * Manual Constructor.
+     * Any setter methods called will save this
+     * object.
+     */
     public Sword()
     {
         this.slot = Slot.HANDS;
     }
 
+    /**
+     * Auto-Loaded Constructor.
+     * @param name Will load any Swords
+     *             with this name, otherwise
+     *             it will create one itself
+     *             with it's given name.
+     */
     public Sword(String name)
     {
         super(name);
--- src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/weapons/Weapon.java
+++ src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/weapons/Weapon.java
@@ -1,14 +1,31 @@
 package main.java.pie.ilikepiefoo2.RPGArcade.Equipment.weapons;
 
+/**
+ *
+ * Used to hold all currently
+ * implemented Weapon types.
+ *
+ */
 public enum Weapon {
     SWORD(Sword.class);
 
     public final Class CLASS;
 
-    Weapon(Class clazz){
-        this.CLASS = clazz;
+    /**
+     * Weapon Enum Constructor
+     * @param CLASS The class this Enum Represents.
+     */
+    Weapon(Class CLASS){
+        this.CLASS = CLASS;
     }
 
+    /**
+     * Used to check implemented Weapon.
+     *
+     * @param className The name of the class
+     * @return boolean  Return true if the className
+     *                  is an implemented Weapon.
+     */
     public static boolean isWeapon(String className)
     {
         for(Weapon weapons : Weapon.values())
