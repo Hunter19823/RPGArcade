72aa73bfb0852a555b27e5df0be3dd0b97ef27fc
Hunter19823 <hspragg@asu.edu>
2020-12-08 17:55:33 -0700

    Made Equipment into array.
    Prevents long switch statements.

--- src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/Equipment.java
+++ src/main/java/pie/ilikepiefoo2/RPGArcade/Equipment/Equipment.java
@@ -4,61 +4,25 @@ package main.java.pie.ilikepiefoo2.RPGArcade.Equipment;
 import java.util.ArrayList;
 
 public class Equipment{
-    private StatModifier HEAD;
-    private StatModifier CHEST;
-    private StatModifier LEGS;
-    private StatModifier BOOTS;
-    private StatModifier RIGHT_HAND;
+    private StatModifier[] EQUIPMENT;
     private ArrayList<StatModifier> PASSIVE;
 
     public Equipment()
     {
         this.PASSIVE = new ArrayList<StatModifier>();
+        this.EQUIPMENT = new StatModifier[Slot.values().length-1];
     }
 
     public StatModifier equip(StatModifier replacement, Slot slot)
     {
         StatModifier currentPiece = null;
-        switch(slot)
+        if(this.EQUIPMENT[slot.POSITION] != null)
         {
-            case HEAD:
-                if(this.HEAD != null)
-                {
-                    currentPiece = this.HEAD;
-                }
-                this.HEAD = replacement;
-                break;
-            case CHEST:
-                if(this.CHEST != null)
-                {
-                    currentPiece = this.CHEST;
-                }
-                this.CHEST = replacement;
-                break;
-            case LEGS:
-                if(this.LEGS != null)
-                {
-                    currentPiece = this.LEGS;
-                }
-                this.LEGS = replacement;
-                break;
-            case FEET:
-                if(this.BOOTS != null)
-                {
-                    currentPiece = this.BOOTS;
-                }
-                this.BOOTS = replacement;
-                break;
-            case HANDS:
-                if(this.RIGHT_HAND != null)
-                {
-                    currentPiece = this.RIGHT_HAND;
-                }
-                this.BOOTS = replacement;
-                break;
+            currentPiece = this.EQUIPMENT[slot.POSITION];
+        }else{
+            this.EQUIPMENT[slot.POSITION] = replacement;
         }
         return currentPiece;
     }
-
 }
 
